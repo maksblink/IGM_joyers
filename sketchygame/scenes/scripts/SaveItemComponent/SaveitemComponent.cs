287c947d70e7d2cb65b72708a7598ec9681967b6
Alicja Fuks <fuksalicja21@gmail.com>
2025-03-28 20:23:56 +0100

    Fixed the solution as requested

--- sketchygame/scenes/scripts/SaveItemComponent/SaveitemComponent.cs
+++ sketchygame/scenes/scripts/SaveItemComponent/SaveitemComponent.cs
@@ -4,22 +4,15 @@ using System;
 public partial class SaveitemComponent : Button
 {
 	public SaveItem Model { get; set; }
-
-	private string SaveIdFormatted
-	{
-		get
-		{
-			string id = Model.SaveId.ToString();
-			return $"{new string('0', 2 - id.Length)}{id}";
-		}
-	}
-
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		TextureRect thumbnailRenderer = GetChild<TextureRect>(0);
-		Text = $"Save {SaveIdFormatted} {Model.LastUsageDate.ToString("dd.MM.YYYY")}";
-		thumbnailRenderer.Texture = Model.Thumbnail;
+		TextureRect thumbnailRect = GetNode<TextureRect>("%ThumbnailRect");
+		Label description = GetNode<Label>("%Description");
+		
+		description.Text = $"Save {Model.SaveIdFormatted} {Model.LastUsageDate.ToString("dd.MM.YYYY")}";
+		thumbnailRect.Texture = Model.Thumbnail;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
--- sketchygame/scenes/scripts/models/SaveItem.cs
+++ sketchygame/scenes/scripts/models/SaveItem.cs
@@ -6,4 +6,13 @@ public class SaveItem
 	public int SaveId { get; set; }
 	public Texture2D Thumbnail { get; set; }
 	public DateTime LastUsageDate { get; set; }
+	
+	public string SaveIdFormatted
+	{
+		get
+		{
+			string id = SaveId.ToString();
+			return $"{new string('0', 2 - id.Length)}{id}";
+		}
+	}
 }
