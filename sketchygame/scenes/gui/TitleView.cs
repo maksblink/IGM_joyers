524b5267f4ce98073352d2d544cc744332f15ffd
Adrian Czubaty <272756@student.pwr.edu.pl>
2025-05-19 18:25:42 +0200

    Added change language button

--- sketchygame/scenes/gui/TitleView.cs
+++ sketchygame/scenes/gui/TitleView.cs
@@ -3,13 +3,23 @@ using Godot;
 namespace SketchyGame.scenes.gui;
 
 public partial class TitleView : Control {
+    private string _lang = "pl";
     public override void _EnterTree() {
         GetTree().Paused = false;
-        
+        GetNode<Button>("%LocaleButton").Text = TranslationServer.GetLocale().ToUpper();
+
         base._EnterTree();
     }
 
 	private void _on_game_exit() {
 		GetTree().Quit();
 	}
+
+    private void _onLocaleButtonPressed() {
+        var localeButton = GetNode<Button>("%LocaleButton");
+        _lang = _lang == "pl" ? "en" : "pl";
+        
+        TranslationServer.SetLocale(_lang);
+        localeButton.Text = _lang.ToUpper();
+    }
 }
