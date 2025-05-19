73fc53ca4ad4ebaff0fb58f68e55438d69e9dad3
Alicja Fuks <fuksalicja21@gmail.com>
2025-05-19 22:51:59 +0200

    Fixed about panel errors

--- sketchygame/scenes/gui/TitleView.cs
+++ sketchygame/scenes/gui/TitleView.cs
@@ -3,18 +3,30 @@ using Godot;
 namespace SketchyGame.scenes.gui;
 
 public partial class TitleView : Control {
-	public override void _EnterTree() {
+	public override void _EnterTree()
+	{
 		GetTree().Paused = false;
 		
 		base._EnterTree();
 	}
 	
-	private void _on_about() {
+	private void _on_about()
+	{
 		Control aboutPanel = GetNode<Control>("%About");
-		aboutPanel.Visible = !aboutPanel.Visible;
+		aboutPanel.Visible = true;
 	}
 
-	private void _on_game_exit() {
+	private void _on_game_exit() 
+	{
 		GetTree().Quit();
+	}
+
+	private void _on_about_outside_click(InputEvent inputEvent)
+	{
+		if (inputEvent is InputEventMouseButton && inputEvent.IsPressed())
+		{
+			Control aboutPanel = GetNode<Control>("%About");
+			aboutPanel.Visible = false;
+		}
 	}
 }
