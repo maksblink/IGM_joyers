a371bd15d08e7c829caac0603b5fbb1312da4554
Alicja Fuks <fuksalicja21@gmail.com>
2025-05-19 21:01:18 +0200

    Added about panel

--- sketchygame/scenes/gui/TitleView.cs
+++ sketchygame/scenes/gui/TitleView.cs
@@ -3,11 +3,16 @@ using Godot;
 namespace SketchyGame.scenes.gui;
 
 public partial class TitleView : Control {
-    public override void _EnterTree() {
-        GetTree().Paused = false;
-        
-        base._EnterTree();
-    }
+	public override void _EnterTree() {
+		GetTree().Paused = false;
+		
+		base._EnterTree();
+	}
+	
+	private void _on_about() {
+		Control aboutPanel = GetNode<Control>("%About");
+		aboutPanel.Visible = !aboutPanel.Visible;
+	}
 
 	private void _on_game_exit() {
 		GetTree().Quit();
