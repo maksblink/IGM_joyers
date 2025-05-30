927b624b77ab2e2381385b1f2f09e925f4e990ea
lipa13 <jaklip04@gmail.com>
2025-05-30 18:42:13 +0200

    poprawiony output i dodanie komentarzy, wciaz nie dziala dla paru obiektow

--- sketchygame/scenes/PolygonGenerator/PolygonGenerator.cs
+++ sketchygame/scenes/PolygonGenerator/PolygonGenerator.cs
@@ -5,6 +5,7 @@ using System.Linq;
 
 public static class PolygonGenerator
 {
+	// delta - minimalna odległość między sąsiadującymi punktami
 	public static Vector2[] GeneratePolygon(Texture2D edgeTexture, float delta)
 	{
 		Image image = edgeTexture.GetImage();
@@ -12,8 +13,14 @@ public static class PolygonGenerator
 		int width = image.GetWidth();
 		int height = image.GetHeight();
 		byte[] data = image.GetData();
-
+		
+		// utworzenie binarnej maski krawędzi
 		Mat edgeMat = new Mat(height, width, MatType.CV_8UC1);
+		
+		// Dla każdego piksela:
+		// - odczytujemy wartość z data[i + 1]
+		// - jeśli > 0, to znaczy, że jest tam krawędź -> ustawiamy piksel na 255 (biały)
+		// - w przeciwnym wypadku → 0 (czarny)
 		for (int y = 0; y < height; y++)
 		{
 			for (int x = 0; x < width; x++)
@@ -24,21 +31,33 @@ public static class PolygonGenerator
 			}
 		}
 
+		// FindContours skanuje binarny obraz (edgeMat) i znajduje wszystkie zamknięte kształty (kontury)
 		Cv2.FindContours(edgeMat, out Point[][] contours, out _, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+		// RetrievalModes.External – szuka tylko konturów zewnętrznych
+		// ApproxSimple – upraszcza kontury (usuwa zbędne punkty w linii prostej)
 
+		// jeśli nie znaleziono konturów -> zwracamy pustą tablicę
 		if (contours.Length == 0)
 			return new Vector2[0];
 
+		// w przeciwnym wypadku -> wybieramy kontur o największym polu powierzchni
 		Point[] largest = contours.OrderByDescending(c => Cv2.ContourArea(c)).First();
 		
+		// próbkowanie punktów co delta
+		
+		// tworzymy listę punktów sampled (będzie naszym polygonem)
 		List<Vector2> sampled = new();
+		// deltaSqr – używamy kwadratu odległości
 		double deltaSqr = delta * delta;
 
+		// dodajemy pierwszy punkt konturu
 		Point prev = largest[0];
 		sampled.Add(new Vector2(prev.X, prev.Y));
-
+		
+		// iterujemy przez kolejne punkty
 		foreach (Point p in largest)
 		{
+			// jeżeli odległość od poprzedniego punktu >= delta, dodajemy go do listy
 			if ((p.X - prev.X) * (p.X - prev.X) + (p.Y - prev.Y) * (p.Y - prev.Y) >= deltaSqr)
 			{
 				sampled.Add(new Vector2(p.X, p.Y));
