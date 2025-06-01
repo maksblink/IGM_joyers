70be786cd7218c27d9c5bbb0eae4686a86fd0b60
Halucyna <263999@student.pwr.edu.pl>
2025-06-01 23:30:56 +0200

    Fix Errors in linux installer (sketchy_recognizer)

--- sketchygame/sketchy_recognizer_installer.sh
+++ sketchygame/sketchy_recognizer_installer.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
 # Get the repository
-git clone https://github.com/maksblink/sketchy_recogniser.git
+if ! find . -type d -name sketchy_recogniser | grep -q sketchy_recogniser; then
+    git clone https://github.com/maksblink/sketchy_recogniser.git
+fi
 
 # Change cwd to project
 cd sketchy_recogniser || exit 1
 
+
+if ! command -v poetry &> /dev/null; then
 # Install poetry
 curl -sSL https://install.python-poetry.org | python3 -
+fi
 
 # Create a new conda environment with Python 3.12.8
-conda create --yes --name .venv --prefix $(pwd) python=3.12.8
+if ! find . -type d -name .venv | grep -q .venv; then
+    conda create --yes --prefix $(pwd)"/.venv" python=3.12.8
+fi
 
 # Get the path to the new Python interpreter
-PYTHON_PATH="$(conda run -n .venv which python)"
+PYTHON_PATH="$(find ./.venv -type f -name python3.12)"
 
 # Set Poetry to use the new Python interpreter
 poetry env use "$PYTHON_PATH"
 
+# Get to the cwd 
+cd ..
+
 # Install dependencies
-poetry install
+poetry --directory ./sketchy_recogniser install --no-root
 
 # Run the main script
-poetry run python main.py
+poetry --directory ./sketchy_recogniser run python main.py
